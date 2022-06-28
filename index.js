9a1683d32b14f1cb6415297b1c09866d28548a52
Abhishek Bhatt <abhishekbhatt625@gmail.com>
2022-06-28 20:45:13 +0545

    creating javascript program

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('hello');
+let x = 9;
+var y = 18;
+var sum = x - y;
+if (sum > 20) {
+  console.log('the number is greater than 20');
+} else {
+  console.log('the nummber is less than 20');
+}
+console.log(sum);
